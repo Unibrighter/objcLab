7e2c6f11f05127275e471bb7e04c90503737c8ed
Kunliang Wu <kunliang@blitzm.com>
2017-10-21 11:54:26 +1100

    First Blood

---
+++ RLObjLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RLObjLearn
+//
+//  Created by Rock Lee on 21/10/2017.
+//  Copyright © 2017 Rock Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RLObjLearn/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RLObjLearn
+//
+//  Created by Rock Lee on 21/10/2017.
+//  Copyright © 2017 Rock Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
