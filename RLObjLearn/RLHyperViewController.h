c118c1d77f5388630f4f4de69ce3c6bf1715c4eb
Rock Lee <rocklee.au@foxmail.com>
2017-11-19 17:06:10 +1100

    Add view controller to the hyper View.

---
+++ RLObjLearn/RLHyperViewController.h
@@ -0,0 +1,14 @@
+//
+//  RLHyperViewController.h
+//  RLObjLearn
+//
+//  Created by Rock Lee on 07/11/2017.
+//  Copyright © 2017 Rock Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface RLHyperViewController : UIViewController
+
+@end
---
+++ RLObjLearn/RLHyperViewController.m
@@ -0,0 +1,25 @@
+//
+//  RLHyperViewController.m
+//  RLObjLearn
+//
+//  Created by Rock Lee on 07/11/2017.
+//  Copyright © 2017 Rock Lee. All rights reserved.
+//
+
+#import "RLHyperViewController.h"
+
+@interface RLHyperViewController()
+
+@end
+
+
+
+@implementation RLHyperViewController
+
+-(void)loadView{
+	
+}
+
+@end
+
+
